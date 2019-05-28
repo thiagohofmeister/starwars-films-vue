ccb874b03b9f3de3b29e0ab4f3dcd4e1409df1b2
Thiago Hofmeister <thiago.souza@moovin.com.br>
2019-05-28 19:41:26 -0300

    Adiciona arquivo de rotas

---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Details from './views/Details.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/details/:episodeId',
+      component: Details
+    }
+  ],
+});

a527741fd20971725bd969cab17ea2fe8f4fe1ce
Thiago Hofmeister <thiago.souza@moovin.com.br>
2019-05-28 19:45:15 -0300

    Adiciona cabeçalho do site, no momento apenas com a Logomarca

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img src="@/assets/logo.png" alt="">
+  </header>
+</template>
+
+<style lang="sass" scoped>
+  header
+    background: #000
+    padding: 20px 0 20px 40px
+
+    img
+      max-width: 160px
+</style>

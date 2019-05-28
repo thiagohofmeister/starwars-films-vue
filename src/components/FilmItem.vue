e45453d55d13cc2c025c37e0c24383a5db28802b
Thiago Hofmeister <thiago.souza@moovin.com.br>
2019-05-28 19:44:50 -0300

    Adiciona componente de itens de filme, serve para preencher a tabela de filmes

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr class="film-item">
+    <td>{{ film.episode_id }}</td>
+    <td>{{ film.title }}</td>
+    <td>
+      <button class="btn btn-info" @click="seeMore">See more</button>
+    </td>
+  </tr>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from "vue-property-decorator"
+  import { Film } from "@/store/models"
+
+  @Component
+  export default class FilmItem extends Vue {
+    @Prop() film?: Film
+
+    seeMore() {
+      this.$router.push(`details/${this.film.episode_id}`)
+    }
+  }
+</script>

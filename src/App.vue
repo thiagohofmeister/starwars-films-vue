0b10542f5663b59a268dd6f198d654dfc400fe8f
Thiago Hofmeister <thiago.souza@moovin.com.br>
2019-05-28 19:40:34 -0300

    Adiciona componente App, este contém o template básico do site

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+
+    <div class="container main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from "vue-property-decorator"
+  import AppHeader from "@/components/AppHeader.vue"
+
+  @Component({
+    components: {
+      AppHeader
+    }
+  })
+  export default class App extends Vue {
+  }
+</script>
+
+<style lang="sass">
+  .main
+    margin-top: 16px
+    margin-bottom: 16px
+</style>
